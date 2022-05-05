3a98f172b8c8104ac68b27e721c43031d7f60efe
Anh Dung <cscssl@gmail.com>
2022-05-05 23:11:45 +0700

    https://dotnettutorials.net/lesson/microservices-using-asp-net-core/

---
+++ SchoolManagementSystem/StudentAdmissionManagement/StudenAdmissionDetailsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentAdmissionManagement
+{
+    public class StudenAdmissionDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; } 
+        public string StudentClass { get; set; }
+        public DateTime DateOfJoining { get; set; }
+    }
+}
