b4b88747729237d9b2ee8c45f0722e0aa20fc097
Anh Dung <cscssl@gmail.com>
2022-05-18 18:30:28 +0700

    Remove unused project

--- OrderProcessing/OSU2SampleWebAPI1/Models/SampleModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OSU2SampleWebAPI1.Models
-{
-    public class SampleModel
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Version { get; set; }
-    }
-}
