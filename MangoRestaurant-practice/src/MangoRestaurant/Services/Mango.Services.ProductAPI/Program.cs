ee99a02169a9338735c5f0f98710f0da063f3874
Anh Dung <cscssl@gmail.com>
2022-05-19 15:32:57 +0700

    Ân ân oán oán

--- MangoRestaurant-practice/src/MangoRestaurant/Services/Mango.Services.ProductAPI/Program.cs
+++ MangoRestaurant-practice/src/MangoRestaurant/Services/Mango.Services.ProductAPI/Program.cs
@@ -1,8 +1,42 @@
+﻿/* 
+ * 
+ * 
+
+<!-- Đức Phật nơi đây xin phù hộ code con chạy không Bug. Nam mô a di đà Phật.
+                           _
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> \"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+                        `=--=-'                    kimcang -->
+Ở đời người ta gặp nhau không ngoài chuyên ân oán, có ân thì báo ân,
+có oán thì báo oán. Vợ chồng cha mẹ anh em là duyên nợ, có duyên thì tụ, hết duyên thì tán...
+
+ * 
+ * 
+ */
+
+
+
 using AutoMapper;
 using Serilog;
 using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.Infrastructures;
 using Microsoft.EntityFrameworkCore;
+using Mango.Services.ProductAPI.Repository;
 
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 
@@ -18,6 +52,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
