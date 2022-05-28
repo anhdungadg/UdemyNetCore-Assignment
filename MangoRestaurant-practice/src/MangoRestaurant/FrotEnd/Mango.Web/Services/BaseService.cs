06d4721750cfa735cab54777d360bf867d935773
Anh Dung <cscssl@gmail.com>
2022-05-29 00:44:39 +0700

    Xong bài 31, vẫn còn lỗi chỗ Dispose()

---
+++ MangoRestaurant-practice/src/MangoRestaurant/FrotEnd/Mango.Web/Services/BaseService.cs
@@ -0,0 +1,80 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDTO responseModel { get; set; }
+
+        public IHttpClientFactory httpClientFactory { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            responseModel = new ResponseDTO();
+            httpClientFactory = httpClient;
+        }
+
+        void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        async Task<T> IBaseService.SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Content-Type", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+
+                if(apiRequest != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch(apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case SD.ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(apiContent);
+
+                return apiResponseDTO;
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDTO
+                {
+                    IsSuccess = false,
+                    DisplayMessage = "Error",
+                    ErrorMessage = new List<string> { ex.ToString() }
+                };
+
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDTO;
+            }
+        }
+    }
+}
---
+++ MangoRestaurant-practice/src/MangoRestaurant/FrotEnd/Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Mango.Web.Models;
+
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        public Task<T> GetAllProductsAsync<T>();
+        public Task<T> GetProductByIdAsync<T>(int id);
+        public Task<T> CreateProductAsync<T>(ProductDTO productDTO);
+        public Task<T> UpdateProductAsync<T>(ProductDTO productDTO);
+        public Task<T> DeleteProductAsync<T>(int id);
+    }
+}
